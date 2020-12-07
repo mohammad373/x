68673bc675aae184a07b4a6ccf71de3d7b53d39e
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-07 21:16:35 +0330

    Update x.py

--- x.py
+++ x.py
@@ -9,3 +9,52 @@ print(Fore.RED + """
 ╚═╝  ╚═╝    ╚══════╝    ╚═╝  ╚═══╝ ╚══╝╚══╝  ╚══╝╚══╝ ╚═════╝ 
                                                               
 """)
+
+baner()
+time.sleep(2)
+
+import os
+import sys
+import time
+import requests
+from colorama import Fore
+def __11__():
+                            try:
+                                # print(Fore.YELLOW + "\nHellow . Welcome Back ;)")
+                                # time.sleep(2)
+                                target = input(Fore.GREEN + "\nEnter Your Address Target" + Fore.YWLLOW + " ==>  ")
+                                time.sleep(2)
+                                if target == "" or None :
+                                    try:
+                                        print(Fore.BLUE + "\nOk Good Lunch ;)")
+                                        time.sleep(2)
+                                        os.system("clear")
+                                        sys.exit()
+                                    except:
+                                        pass
+                                r = requests.get("http://" + target + "/wp-content/plugins/")
+                                if r.status_code == 404 or r.status_code == 500:
+                                    try:
+                                        time.sleep(2)
+                                        print(Fore.YELLOW + "\n[!] - Your Target Testing ...")
+                                        time.sleep(2)
+                                        print(Fore.YELLOW + "\n[!] - Pleass Wait ...")
+                                        print(Fore.RED + "\n[-] - Your Target Is Bot WordPress ;(")
+                                        time.sleep(2)
+                                        sys.exit()
+                                    except:
+                                        pass
+                                else:
+                                    try:
+                                        time.sleep(2)
+                                        print(Fore.YELLOW + "\n[!] - Your Target Testing ...")
+                                        time.sleep(2)
+                                        print(Fore.YELLOW + "\n[!] - Pleass Wait ...")
+                                        print(Fore.GREEN + "\n[+] - Your Target Is WordPress ; ")
+                                        time.sleep(2)
+                                        sys.exit()
+                                    except:
+                                        pass
+                            except:
+                                pass
+__11__()
